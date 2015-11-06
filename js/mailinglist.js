a6c6bdb824b48e8f49f60a0fd8cadef011b0af32
Ethan Liang <ethanliang97@gmail.com>
2015-11-06 01:20:31 -0500

    Finished Contact page, added progress bar, added under construction

--- js/mailinglist.js
+++ js/mailinglist.js
@@ -1,7 +1,12 @@
 angular.module('mailinglist', [])
-    .controller("index", ["$scope", function ($scope) {
+    .controller("progress", ["$scope", function ($scope) {
     $scope.name;
-        
-        
+    $scope.email; 
+    $scope.phone;
+    $scope.message; 
+    $scope.namecomplete = function (){return $scope.name!=null;};    
+    $scope.emailcomplete = function (){return $scope.email!=null;}; 
+    $scope.phonecomplete = function (){return $scope.phone!=null;}; 
+    $scope.messagecomplete = function (){return $scope.message!=null;}; 
         
     }]);
