e02a276a2d17dc2e58f4ad7de7590e07616bcc3b
Ethan Liang <ethanliang97@gmail.com>
2015-11-09 14:22:20 -0500

    Added ng-repeat methodology for contact form and shrunk code

--- js/mailinglist.js
+++ js/mailinglist.js
@@ -1,17 +1,12 @@
-angular.module('mailinglist', [])
-    .controller("progress", ["$scope", function ($scope) {
-    $scope.name;
-    $scope.email; 
-    $scope.phone;
-    $scope.message;
-    $scope.subscribeinput; 
-    $scope.namecomplete = function (){return $scope.name!=null;};    
-    $scope.emailcomplete = function (){return $scope.email!=null;}; 
-    $scope.phonecomplete = function (){return $scope.phone!=null;}; 
-    $scope.messagecomplete = function (){return $scope.message!=null;}; 
-
-        
-    }]);
+angular.module('formfield',[])
+    .controller('formcontroller',function() {
+    this.formstofill = [{Field:'Name', Value:''},{Field:'Email', Value:''}, {Field:'Phone', Value:''}, {Field:"Message", Value:''}];
+    this.filled= function filled(progress)
+    { return (progress.Value!=''); 
+    };
+    
+    
+});
 
 //Firebase management for subscription
 function submitfunc(){
