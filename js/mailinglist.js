47ca3cafc1f5eb18ffeb7ce3b9bbc614d18fa316
Ethan Liang <ethanliang97@gmail.com>
2015-11-09 22:44:19 -0500

    Linked firebase and changed formatting

--- js/mailinglist.js
+++ js/mailinglist.js
@@ -1,23 +1,24 @@
 var mailapp=angular.module('formfield',["firebase"]);
-    mailapp.controller('formcontroller',['$scope','$firebaseObject', function($scope, $firebaseObject) {
-    $scope.formstofill = [{Field:'Name', Value:''},{Field:'Email', Value:''}, {Field:'Phone', Value:''}, {Field:"Message", Value:''}];
-    $scope.filled= function filled(progress)
-    { return (progress.Value!='');
-    };
-    $scope.subscribe = true; 
-    $scope.firesync = [{}];   
+    mailapp.controller('formcontroller',['$scope','$firebaseArray', function($scope, $firebaseArray) {
+        var ref = new Firebase("https://dazzling-heat-2850.firebaseio.com/");   
+        $scope.firesync = $firebaseArray(ref);
+        $scope.formstofill = [{Field:'Name', Value:''},{Field:'Email', Value:''}, {Field:'Phone', Value:''}, {Field:"Message", Value:''}];
+        $scope.filled= function filled(progress){
+            return (progress.Value!='');
+        };
+        $scope.subscribe = true;
+        $scope.submitfunction =function(){
+            $scope.formstofill.push($scope.subscribe);
+            window.alert("Thanks for Leaving a Message!");
+            $scope.firesync.$add($scope.formstofill);    
+            $scope.formstofill.pop();
+            angular.forEach($scope.formstofill, function(value,key){
+                value.Value='';
+                
+            }) 
         
-        
-        
-    var ref = new Firebase("https://dazzling-heat-2850.firebaseio.com/");   
-    var fireobj = $firebaseObject(ref);
-
-        
-    $scope.submitfunction = function submitfunction(information){
-        fireobj.$bindTo($scope,"firesync");
-
-        
-    }; 
+    };      
+    
     
     
 }]);
