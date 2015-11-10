3cea5725a1d5b6a2b5210769cf64d7536bfa9c78
Ethan Liang <ethanliang97@gmail.com>
2015-11-09 22:48:44 -0500

    Removed old firebase script

--- js/mailinglist.js
+++ js/mailinglist.js
@@ -22,14 +22,3 @@ var mailapp=angular.module('formfield',["firebase"]);
     
     
 }]);
-
-////Firebase management for subscription
-//function submitfunc(){
-//var myDataRef = new Firebase('https://dazzling-heat-2850.firebaseio.com/');
-//var name = $('#name').val();
-//var email = $('#email').val();
-//var phonenumber = $('#phone').val(); 
-//var message = $('#message').val();
-////var subscribe = $('#subscribe').val();
-//myDataRef.push({name: name, email: email, phonenumber: phone, message: message});
-//};
