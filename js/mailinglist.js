92f7da4ab14d6f06e7623fc082feb4046571d424
Ethan Liang <ethanliang97@gmail.com>
2015-11-09 21:20:08 -0500

    Adding AngularFire module

--- js/mailinglist.js
+++ js/mailinglist.js
@@ -1,20 +1,34 @@
-angular.module('formfield',[])
-    .controller('formcontroller',function() {
-    this.formstofill = [{Field:'Name', Value:''},{Field:'Email', Value:''}, {Field:'Phone', Value:''}, {Field:"Message", Value:''}];
-    this.filled= function filled(progress)
-    { return (progress.Value!=''); 
+var mailapp=angular.module('formfield',["firebase"]);
+    mailapp.controller('formcontroller',['$scope','$firebaseObject', function($scope, $firebaseObject) {
+    $scope.formstofill = [{Field:'Name', Value:''},{Field:'Email', Value:''}, {Field:'Phone', Value:''}, {Field:"Message", Value:''}];
+    $scope.filled= function filled(progress)
+    { return (progress.Value!='');
     };
-    
-    
-});
+    $scope.subscribe = true; 
+    $scope.firesync = [{}];   
+        
+        
+        
+    var ref = new Firebase("https://dazzling-heat-2850.firebaseio.com/");   
+    var fireobj = $firebaseObject(ref);
 
-//Firebase management for subscription
-function submitfunc(){
-var myDataRef = new Firebase('https://dazzling-heat-2850.firebaseio.com/');
-var name = $('#name').val();
-var email = $('#email').val();
-var phonenumber = $('#phone').val(); 
-var message = $('#message').val();
-//var subscribe = $('#subscribe').val();
-myDataRef.push({name: name, email: email, phonenumber: phone, message: message});
-};
+        
+    $scope.submitfunction = function submitfunction(information){
+        fireobj.$bindTo($scope,"firesync");
+
+        
+    }; 
+    
+    
+}]);
+
+////Firebase management for subscription
+//function submitfunc(){
+//var myDataRef = new Firebase('https://dazzling-heat-2850.firebaseio.com/');
+//var name = $('#name').val();
+//var email = $('#email').val();
+//var phonenumber = $('#phone').val(); 
+//var message = $('#message').val();
+////var subscribe = $('#subscribe').val();
+//myDataRef.push({name: name, email: email, phonenumber: phone, message: message});
+//};
